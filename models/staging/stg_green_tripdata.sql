95afc46a1bb354695229edf0f544621bc508d497
timkashar@gmail.com <timkashar@gmail.com>
2023-02-25 21:53:10 +0000

    successfull

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,7 +1,12 @@
-{{ config(materialized='view')}}
+{{ config(materialized='view') }}
 
-
-
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green_tripdata') }}
+  where vendorid is not null 
+)
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
@@ -30,11 +35,11 @@ select
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
-    {{ get_payment_type_description('payment_type')}} as payment_type_description,
+    {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
+from tripdata
+where rn = 1
 
-from {{source('staging', 'green_tripdata')}} 
-where vendorid is not null
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
